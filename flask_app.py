5ad6bba3c738297275e8a6c96010ec248b154265
indresh-singh <indresh_singh@mckinsey.com>
2022-09-15 09:54:48 +0530

    html

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.update(
+    TESTING=True,
+    SECRET_KEY="ops_digital_asset"
+)
 
-@app.route("/")
-def hello_world():
-    return "<h2> This is the first web app </h2>"
+@app.route("/", methods=['GET','POST'])
+def login():
+    user ={"name":"Ajay"}
+    return render_template("index.html", user=user)
 
 if __name__ == "__main__":
     app.run()
